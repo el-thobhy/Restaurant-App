a3f1de6bd16ff315b5da86a51c8cc59efd70c488
Auriwan Yasper <79439774+auriwan@users.noreply.github.com>
2022-09-06 17:37:55 +0700

    detail and search test

--- lib/core/data/models/search/search_response.dart
+++ lib/core/data/models/search/search_response.dart
@@ -13,22 +13,20 @@ class SearchResponse extends Equatable {
       required this.count,
       required this.result});
 
-  factory SearchResponse.fromJson(Map<String, dynamic> json) =>
-      SearchResponse(
-          error: json['error'],
-          message: '',
-          result: List<SearchModel>.from(
-              json['restaurants'].map((e) => SearchModel.fromJson(e))),
-          count: json['founded']);
-
-  factory SearchResponse.fromJsonNo() => const SearchResponse(
-        error: true,
-        message:
-            "No Internet Connection\nPlease check your internet connection",
-        result: [],
-        count: 0,
+  factory SearchResponse.fromJson(Map<String, dynamic> json) => SearchResponse(
+        error: json['error'],
+        message: '',
+        result: List<SearchModel>.from(
+            json['restaurants'].map((e) => SearchModel.fromJson(e))),
+        count: json['founded'],
       );
 
+  Map<String, dynamic> toJson() => {
+        "error": error,
+        "message": message,
+        "result": List<dynamic>.from(result.map((e) => e.toJson())),
+        "count": count,
+      };
   @override
   List<Object> get props => [error, message, result];
 }
